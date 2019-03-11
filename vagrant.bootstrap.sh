fffdace10cd06169b0a255f8a75d1638c2b70c17
perk <marcin.stozek@gmail.com>
2019-03-11 21:31:09 +0100

    Nie używaj Dockera z repo Ubuntu tylko bezpośrednio z repozytoriów Dockera

--- vagrant.bootstrap.sh
+++ vagrant.bootstrap.sh
@@ -12,15 +12,31 @@ export LANG=en_US.UTF-8
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-
 apt-get install --yes \
-  docker.io \
-  docker-compose \
   curl \
   vim \
   git
 
+# Install Docker
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-18-04
+apt-get install --yes \
+  apt-transport-https \
+  ca-certificates curl \
+  software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
+apt-get update
+apt-get install --yes \
+  docker-ce
+
 usermod -aG docker vagrant
+
+# Install docker-compose
+# https://www.digitalocean.com/community/tutorials/how-to-install-docker-compose-on-ubuntu-18-04
+curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 echo
 echo
